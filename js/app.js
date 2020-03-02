e0f65a6ed3a694d17554bc1b964e3027ebb4e5a0
vanduc2514 <vanduc2514@gmail.com>
2020-03-02 23:19:49 +0700

    Sửa thuật toán của hàm đệ quy

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
-function isPalindrome(string) {
-    if (string.length <= 1) {
-        return true;
-    }
-    if (string[0] === string[string.length -1]) {
-        string = string.slice(0, -1);
-        string = string.substr(1);
-        isPalindrome(string);
-    }
-    return false;
-}
-
 function startFunc() {
     let value = prompt("Nhập vào chuỗi muốn kiểm tra: ");
     if (isPalindrome(value)) {
         return alert("Chuỗi đối xứng");
     }
     return alert("Chuỗi không đối xứng");
+}
+
+function isPalindrome(string) {
+    if (string.length <= 1) {
+        return true;
+    }
+    if (string[0] == string[string.length - 1]) {
+        string = string.slice(0, -1);
+        string = string.substr(1);
+        return isPalindrome(string); //Chỗ này thêm return mới có giá trị trả về
+    }
+    return false;
 }
